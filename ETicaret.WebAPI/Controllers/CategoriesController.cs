c2be90a799807416fc98cb702b2efc18b61aa5d1
Batuhan Inal <np_batu@hotmail.com>
2022-12-30 01:44:17 +0300

    Category controller added

---
+++ ETicaret.WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using ETicaret.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var categories = _categoryService.GetAll();
+            if (categories.Success)
+            {
+                return Ok(categories);
+            }
+            return BadRequest(categories);
+        }
+    }
+}
